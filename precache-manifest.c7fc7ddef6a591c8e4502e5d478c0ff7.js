5054427604b4d3282ff37d893c95af663d2e98fe
elviajero971 <lucas.illiano@hotmail.com>
2020-09-03 00:38:28 +0200

    Updates

---
+++ precache-manifest.c7fc7ddef6a591c8e4502e5d478c0ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76f5bffa41775faaf294ff354f1c568",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "67b534bf1f18d78dd1ea",
+    "url": "/todo-app-react/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "a7c31d6321deb2f9c96a",
+    "url": "/todo-app-react/static/js/2.3825438a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.3825438a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b534bf1f18d78dd1ea",
+    "url": "/todo-app-react/static/js/main.7967a860.chunk.js"
+  },
+  {
+    "revision": "6ff3bc5643e66aaf2ca4",
+    "url": "/todo-app-react/static/js/runtime-main.c151e871.js"
+  }
+]);
