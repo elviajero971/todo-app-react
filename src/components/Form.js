ea969fbd76b71e1b11d64d9c602889c8e89cce85
elviajero971 <lucas.illiano@hotmail.com>
2020-09-02 23:46:14 +0200

    last update from tutorial

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Form = ({ setInputText, todos, setTodos, inputText}) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus}) => {
   //Here I can write javascript code and function
   const inputTextHandler = (e) => {
     console.log(e.target.value);
@@ -11,17 +11,27 @@ const Form = ({ setInputText, todos, setTodos, inputText}) => {
   const submitTodoHandler = (e) => {
     e.preventDefault();
     setTodos([
-      ...todos, {text: inputText, completed: false, id: Math.random() * 100}
+      ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
     ]);
+    setInputText("");
   };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  }
   return(
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <input 
+        value={inputText} 
+        onChange={inputTextHandler} 
+        type="text" 
+        className="todo-input" 
+      />
       <button onClick = {submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
